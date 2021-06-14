b6c7c9ae869ea7421fe2532c43bbb8557c42d089
Majid Ali Khan <51267704+majidalykhan@users.noreply.github.com>
2021-06-14 15:43:28 +0500

    Project Created

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, {useState} from "react";
+// import data from './data';
+// import list from './list';
+
+
+function App() {
+  return <h2>Birthday Reminder</h2>;
+}
+
+export default App;
