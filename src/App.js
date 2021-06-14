fcee3f6bbff20e2068003ff77d92bc0a74d81fe9
Majid Ali Khan <51267704+majidalykhan@users.noreply.github.com>
2021-06-14 16:22:19 +0500

    Data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, {useState} from "react";
-import Data from './Data';
+import data from './Data';
 import List from './List';
 
 
 function App() {
+
+  const [people, setPeople] = useState(data);
+
   return <main>
     <section className='container'>
-      <h3>0 Birthdays Today</h3>
-      <List/>
+      <h3> {people.length} Birthdays Today</h3>
+      <List people={people} />
       <button onClick={() => console.log('You Clicked Me')}>Clear All</button>
     </section>
   </main>;
---
+++ src/Data.js
@@ -0,0 +1,30 @@
+export default [
+
+    {
+        id: 1,
+        name: 'John',
+        age:21,
+        image: '<img src="https://img.icons8.com/color/96/000000/circled-user-male-skin-type-4--v1.png"/>',
+    },
+
+    {
+        id: 2,
+        name: 'Tom',
+        age:27,
+        image: '<img src="https://img.icons8.com/color/96/000000/circled-user-male-skin-type-4--v1.png"/>',
+    },
+
+    {
+        id: 3,
+        name: 'Jim',
+        age:25,
+        image: '<img src="https://img.icons8.com/color/96/000000/circled-user-male-skin-type-4--v1.png"/>',
+    },
+
+    {
+        id: 4,
+        name: 'Daniel',
+        age:24,
+        image: '<img src="https://img.icons8.com/color/96/000000/circled-user-male-skin-type-4--v1.png"/>',
+    },
+]
