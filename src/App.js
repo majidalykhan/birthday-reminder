7c3d9f1b89f598154db8ce5e75162ca46d7d1aad
Majid Ali Khan <majidalykhan@gmail.com>
2022-08-05 18:14:44 +0500

    Birthday reminder complete
    Populated list with static data, used props and useState in React.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import React, { useState } from "react";
+import List from "./List";
+import data from "./data";
+
 function App() {
+  const [people, setPeople] = useState(data);
+
   return (
-    <div className="patterns pt1">
-      <h2>Birthday Reminder</h2>
-    </div>
+    <main>
+      <section className="container">
+        <h3>{people.length} birthdays today</h3>
+        <List people={people} />
+        <button type="button" onClick={() => setPeople([])}>
+          Clear All
+        </button>
+      </section>
+    </main>
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,32 @@
+export default [
+  {
+    id: 1,
+    name: "Harry",
+    age: 22,
+    image: "https://img.icons8.com/bubbles/100/000000/user-male-circle.png",
+  },
+  {
+    id: 2,
+    name: "Rachel",
+    age: 25,
+    image: "https://img.icons8.com/bubbles/100/000000/user-female.png",
+  },
+  {
+    id: 3,
+    name: "Owen",
+    age: 29,
+    image: "https://img.icons8.com/bubbles/100/000000/user-male.png",
+  },
+  {
+    id: 4,
+    name: "Tom",
+    age: 16,
+    image: "https://img.icons8.com/bubbles/100/000000/guest-male.png",
+  },
+  {
+    id: 5,
+    name: "Amelia",
+    age: 23,
+    image: "https://img.icons8.com/bubbles/100/000000/amy-winehouse.png",
+  },
+];
